67bfdf33d98d9dafe29da7ec22508194e782e391
OverMan-647 <brunoadji@id.uff.br>
2023-05-24 19:46:59 -0300

    Changing struct of Trie

--- trie.c
+++ trie.c
@@ -1,4 +1,10 @@
-typedef struct {
+//characters allowed: abcdefghijklmnopqrstuvwxyz0123456789_
+//amount of characters allowed
+#define SIZE 37
+
+typedef struct TrieNode {
+    struct TrieNode* children[SIZE];
     float value;
     char* key;
+    int isLeaf;
 } Trie;
