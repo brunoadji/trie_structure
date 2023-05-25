8178a55783a3ba6b33381075ed38d0190608825c
OverMan-647 <brunoadji@id.uff.br>
2023-05-24 21:48:59 -0300

    Implementing createNode and start of insertion

--- trie.c
+++ trie.c
@@ -1,5 +1,7 @@
 //characters allowed: abcdefghijklmnopqrstuvwxyz0123456789_
 //amount of characters allowed
+#include <stdio.h>;
+#include <stdlib.h>;
 #define SIZE 37
 
 typedef struct TrieNode {
@@ -8,3 +10,32 @@ typedef struct TrieNode {
     char* key;
     int isLeaf;
 } Trie;
+
+Trie* triInsert(Trie* root, char* key, float value, int index) {
+    for(int i = 0; i < SIZE; i++) {
+        if(root->children[i] == key[index]) {
+
+        } else if(root->children[i] == NULL) {
+
+        }
+    }
+    return root;
+}
+
+Trie* createNode(char* key, float value) {
+    Trie* newNode = (Trie*) malloc(sizeof(Trie));
+    newNode->isLeaf = 1;
+    newNode->key = key;
+    newNode->value = value;
+
+    //starting all children as NULL
+    for(int i = 0; i < SIZE; i++) {
+        newNode->children[i] = NULL;
+    }
+
+    return newNode;
+}
+
+void main() {
+    printf("%d", 1==0);
+}
