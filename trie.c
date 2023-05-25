2f15df29356ae3de2aa5cb87f9a56ce98a3e3755
pedro-emanuel-am <pedroemanuel@id.uff.br>
2023-05-24 22:55:04 -0300

    busca palavra
    função inicial para busca; busca se uma palavra esta na estrutura

--- trie.c
+++ trie.c
@@ -8,3 +8,18 @@ typedef struct TrieNode {
     char* key;
     int isLeaf;
 } Trie;
+
+int trie_busca(Trie* no, char* word){
+	//if(no == NULL)
+		//return 0; //retorna 0 se nó vazio
+	struct Trie* aux = no;
+	for(int i = 0; word[i]!='\0'; i++){ //for para mover posiçao da string
+		int letra = word[i] - 'a'; //armazena o valor da posicao
+		if(aux->children[letra] == NULL) //checa se no existe
+			return 0;
+		aux = aux->children[letra]; //move para filho
+	}
+	if(aux != NULL && aux->isLeaf == 1) //retorna se no é folha
+		return 1;
+	return 0;
+}
--- trie.h
+++ trie.h
@@ -1,2 +1,3 @@
 int tri_insert(char* key, float value);
+int trie_busca(Trie* no, char* word)
 float search_key(char* key);
