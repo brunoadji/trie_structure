b5378a886bc0c63cb313b09ddfb2e4e52ba81432
OverMan-647 <brunoadji@id.uff.br>
2023-06-16 16:13:51 -0300

    Fixing identation + adding type create

--- server.py
+++ server.py
@@ -7,14 +7,17 @@ cors = CORS(app)
 
 @app.route("/receiver", methods=["POST"])
 def postME():
-   data = request.get_json()
-   print(data)
-   print(data["type"])
+    data = request.get_json()
+    print(data)
+    print(data["type"])
 
-   if data["type"] == "transaction":
-       res = "Transferindo de " + data["from"] + " para " + data["to"] + " a quantidade de " + data["amount"]
+    if data["type"] == "transaction":
+        res = "Transferindo de " + data["from"] + " para " + data["to"] + " a quantidade de " + data["amount"]
+    
+    if data["type"] == "create":
+        res = "Criando a conta " + data["account"] + " com o saldo de " + data["amonut"]
 
-   return jsonify(res)
+    return jsonify(res)
 
 if __name__ == "__main__":
-     app.run()
+    app.run()
