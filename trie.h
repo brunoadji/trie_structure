e001a7260eec0aac2513bcc3f863c8be721d53bc
OverMan-647 <brunoadji@id.uff.br>
2023-06-17 11:49:32 -0300

    Adding pointer

--- trie.h
+++ trie.h
@@ -1,4 +1,4 @@
 int triInsert(char* key, float value);
-float triSearch(char* word, float value);
+int triSearch(char* word, float *value);
 int triRemove(char* key);
 int triUpdate(char* word, float newValue);
