f426d01ea5db86c008cbb1040805413ea6efcb9c
brunoadji <brunoadji@id.uff.br>
2023-06-17 16:14:07 -0300

    Refactor

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import math
+from ctypes import *
 
 app = Flask(__name__)
 cors = CORS(app)
 
 @app.route("/receiver", methods=["POST"])
-def postME():
+def post():
     data = request.get_json()
     print(data)
     print(data["type"])
 
     if data["type"] == "transaction":
         res = "Transferindo de " + data["from"] + " para " + data["to"] + " a quantidade de " + data["amount"]
-    
+
     if data["type"] == "create":
         res = "Criando a conta " + data["account"] + " com o saldo de " + data["amount"]
 
